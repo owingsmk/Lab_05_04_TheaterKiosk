ff0569e83121f6c1218e8ffd913bcda4a0cbfabe
Mark Owings <owingsmk@mail.uc.edu>
2023-02-12 16:36:28 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 21; //Normally this is the variable setup
+        int ADULT=21; //Variable setup for adult in the program
+        System.out.println("Enter your age "); //Output to the user
+        //Input age
+        if (age>=ADULT){
+            System.out.println("You get a wrist band. Have a wonderful time. ");
+        }
+
+    }
+}
