fc187499496aaea70df3b09885fdd5428244bc0a
Mark Owings <owingsmk@mail.uc.edu>
2023-02-12 16:38:34 -0500

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        int age = 21; //Normally this is the variable setup
+        int age = 18; //Normally this is the variable setup
         int ADULT=21; //Variable setup for adult in the program
         System.out.println("Enter your age "); //Output to the user
         //Input age
-        if (age>=ADULT){
+        if (age>=ADULT){ //if they are an adult
             System.out.println("You get a wrist band. Have a wonderful time. ");
         }
-
+        //No output if they are under 21
     }
 }
